44136132453d06a6fc742f16d357d068be3d95de
Raj <rajashekharms369@gmail.com>
2023-05-30 11:18:34 +0530

    initial commit

---
+++ src/main/java/com/example/HomeController.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello(){
+        return new ResponseEntity<>("Hello world", HttpStatus.OK);
+    }
+}
